f4f8029e677ddc432fcef81eb1bd90bef029dca4
Dominique Unruh <unruh@ut.ee>
2021-01-21 14:04:30 +0200

    true lvalues are now a subset of operators on maps

--- CatLValue.thy
+++ CatLValue.thy
@@ -1,19 +1,29 @@
 theory CatLValue
-  imports CatLValue_Axioms2
+  imports CatLValue_Axioms1
 begin
 
 definition 
-  \<open>compatible x y \<longleftrightarrow> (\<forall>f g. (x \<cdot> f) o\<^sub>m (y \<cdot> g) = (y \<cdot> g) o\<^sub>m (x \<cdot> f))\<close>
+  \<open>compatible x y \<longleftrightarrow> (is_lvalue x \<and> is_lvalue y \<and> (\<forall>f g. (x \<cdot> f) o\<^sub>m (y \<cdot> g) = (y \<cdot> g) o\<^sub>m (x \<cdot> f)))\<close>
 
 (* definition
   \<open>pair x y = (THE xy. \<forall>f g. xy \<cdot> (f \<otimes> g) = (x \<cdot> f) o\<^sub>m (y \<cdot> g))\<close> *)
 
-lemma pair_apply[simp]:
+(* lemma pair_apply[simp]:
   assumes "compatible x y"
   shows "pair x y \<cdot> (f \<otimes> g) = (x \<cdot> f) o\<^sub>m (y \<cdot> g)"
-  apply (rule pair_apply0) using assms by (simp add: compatible_def)
+  apply (rule pair_apply0) using assms by (simp add: compatible_def) *)
+
+declare pair_apply[simp]
+declare lvalue_id[simp]
+declare lvalue_comp_app[simp]
+declare lvalue_tensor_app[simp]
+declare left_tensor_apply[simp]
+declare swap_apply[simp]
+declare assoc_apply[simp]
+declare assoc'_apply[simp]
 
 lemma compatibleI:
+  assumes "is_lvalue x" and "is_lvalue y"
   assumes \<open>\<And>f g. (x \<cdot> f) o\<^sub>m (y \<cdot> g) = (y \<cdot> g) o\<^sub>m (x \<cdot> f)\<close>
   shows "compatible x y"
   using assms unfolding compatible_def by simp
@@ -39,13 +49,10 @@ lemma lvalue_app_fun_cong: "f = g \<Longrightarrow> f \<cdot> x = g \<cdot> x" b
 lemma compatible3:
   assumes [simp]: "compatible x y" and "compatible y z" and "compatible x z"
   shows "compatible (pair x y) z"
-  apply (rule compatibleI)
-  apply (rule compatible30)
-  using assms unfolding compatible_def by auto
-(* proof -
+proof (rule compatibleI)
   have "pair (pair x y) z \<cdot> (f \<otimes> g) \<otimes> h = pair z (pair x y) \<cdot> h \<otimes> (f \<otimes> g)" for f g h
     apply auto using assms unfolding compatible_def
-    by (metis maps_comp_assoc)k
+    by (metis maps_comp_assoc)
   then have "(pair (pair x y) z o\<^sub>l swap o\<^sub>l left_tensor h) \<cdot> (f \<otimes> g)
            = (pair z (pair x y) o\<^sub>l left_tensor h) \<cdot> (f \<otimes> g)" for f g h
     by auto
@@ -56,17 +63,20 @@ lemma compatible3:
            = (pair z (pair x y)) \<cdot> (h \<otimes> fg)" for fg h
     using *[THEN lvalue_app_fun_cong]
     by auto
-  then show ?thesis
+  then show "(pair x y \<cdot> fg) o\<^sub>m (z \<cdot> h) = (z \<cdot> h) o\<^sub>m (pair x y \<cdot> fg)" for fg h
     unfolding compatible_def by simp
-qed *)
+  show "is_lvalue z"
+    using assms(2) compatible_def by auto
+  show "is_lvalue (pair x y)" 
+    by (meson assms(1) compatible_def pair_is_lvalue0)
+qed
 
-lemma compatible_chain_left: "compatible x y \<Longrightarrow> compatible (chain x z) y"
-  unfolding compatible_def chain_def lvalue_comp_app by auto
+lemma compatible_chain_left: "compatible x y \<Longrightarrow> is_lvalue z \<Longrightarrow> compatible (chain x z) y"
+  by (auto simp: chain_def comp_is_lvalue compatible_def)
   
 lemma compatible_chain_inner: 
-  "compatible x y \<Longrightarrow> compatible (chain z x) (chain z y)"
-  unfolding compatible_def chain_def  
-  by (auto simp flip: lvalue_app_mult)
+  "compatible x y \<Longrightarrow> is_lvalue z \<Longrightarrow> compatible (chain z x) (chain z y)"
+  by (auto simp flip: lvalue_app_mult simp: compatible_def chain_def comp_is_lvalue)
 
 definition is_everything where
   "is_everything x \<longleftrightarrow> (\<exists>y. y o\<^sub>l x = 1\<^sub>l)"
@@ -103,13 +113,15 @@ proof (simp add: is_complement_def, rule)
   have xx'[simp]: "compatible x x'" and yy'[simp]: "compatible y y'"
     and x'x[simp]: "compatible x' x" and y'y[simp]: "compatible y' y"
     using assms is_complement_def compatible_sym by blast+
+  then have [simp]: "is_lvalue x" "is_lvalue x'" "is_lvalue y" "is_lvalue y'"
+    using compatible_def by blast+
   show compat: "compatible (chain x y) (pair (chain x y') x')"
     apply (rule compatible_sym)
     apply (rule compatible3)
-      apply (rule compatible_chain_left, simp)
+      apply (rule compatible_chain_left, simp, simp)
      apply (rule compatible_sym)
-    apply (rule compatible_chain_left, simp)
-    by (rule compatible_chain_inner, simp)
+    apply (rule compatible_chain_left, simp, simp)
+    by (rule compatible_chain_inner, simp_all)
   from assms(1)
   obtain xInv where xInv: "xInv \<cdot> pair x x' \<cdot> f = f" for f
     unfolding is_complement_def is_everything_def
@@ -124,17 +136,15 @@ proof (simp add: is_complement_def, rule)
   proof -
     have "pair (chain x y) (pair (chain x y') x') \<cdot> (f \<otimes> (g \<otimes> h))
       = (x \<cdot> y \<cdot> f) o\<^sub>m (x \<cdot> y' \<cdot> g) o\<^sub>m (x' \<cdot> h)"
-      apply (subst pair_apply, rule compat)
-      apply (subst pair_apply, simp add: compatible_chain_left)
       by (simp add: maps_comp_assoc chain_def)
     also have "\<dots> = (x \<cdot> (pair y y') \<cdot> (f \<otimes> g)) o\<^sub>m (x' \<cdot> h)"
       by (simp add: lvalue_app_mult)
     also have "\<dots> = (pair x x') \<cdot> ((pair y y' \<cdot> (f \<otimes> g)) \<otimes> h)"
       by simp
     also have "xInv \<cdot> ... = ((pair y y' \<cdot> (f \<otimes> g)) \<otimes> h)"
-      using xInv apply simp by (metis pair_apply[OF xx'])
+      using xInv apply simp by (metis pair_apply)
     also have "lvalue_tensor yInv 1\<^sub>l \<cdot> \<dots> = (f \<otimes> g) \<otimes> h"
-      using yInv apply simp by (metis pair_apply[OF yy'])
+      using yInv apply simp by (metis pair_apply)
     also have "assoc' \<cdot> \<dots> = f \<otimes> (g \<otimes> h)"
       by simp
     finally show ?thesis
--- CatLValue_Axioms2.thy
+++ CatLValue_Axioms2.thy
@@ -26,43 +26,51 @@ axiomatization
                \<Rightarrow> ('a,'b,'c,'d) lvalue \<Rightarrow> ('a,'b,'e,'f) lvalue" (infixl "o\<^sub>l" 55) and
   lvalue_tensor :: "('a,'b,'c,'d) lvalue \<Rightarrow> ('e,'f,'g::domain,'h::domain) lvalue \<Rightarrow> ('a\<times>'e,'b\<times>'f,'c\<times>'g,'d\<times>'h) lvalue"
 
+axiomatization is_lvalue :: "('a::domain, 'b::domain, 'c::domain, 'd::domain) lvalue \<Rightarrow> bool"
+
 axiomatization where
-  lvalue_id[simp]: "1\<^sub>l \<cdot> x = x"
+  comp_is_lvalue: "is_lvalue x \<Longrightarrow> is_lvalue y \<Longrightarrow> is_lvalue (x o\<^sub>l y)"
+
 axiomatization where
-  lvalue_comp_app[simp]: "(x o\<^sub>l y) \<cdot> f = x \<cdot> y \<cdot> f"
-(* TODO: only for a subset of lvalues *)
+  lvalue_id: "1\<^sub>l \<cdot> x = x"
 axiomatization where
-  lvalue_app_mult: "x \<cdot> (f o\<^sub>m g) = (x \<cdot> f) o\<^sub>m (x \<cdot> g)"
+  lvalue_comp_app: "(x o\<^sub>l y) \<cdot> f = x \<cdot> y \<cdot> f"
+axiomatization where
+  lvalue_app_mult: "is_lvalue x \<Longrightarrow> x \<cdot> (f o\<^sub>m g) = (x \<cdot> f) o\<^sub>m (x \<cdot> g)"
 axiomatization where
   maps_comp_assoc: "(f o\<^sub>m g) o\<^sub>m h = f o\<^sub>m (g o\<^sub>m h)"
 axiomatization where
-  lvalue_tensor_app[simp]: "(lvalue_tensor x y) \<cdot> (f \<otimes> g) = (x \<cdot> f) \<otimes> (y \<cdot> g)"
+  lvalue_tensor_app: "(lvalue_tensor x y) \<cdot> (f \<otimes> g) = (x \<cdot> f) \<otimes> (y \<cdot> g)"
 
 axiomatization where tensor_extensionality:
   "(\<And>f g. x \<cdot> (f \<otimes> g) = y \<cdot> (f \<otimes> g)) \<Longrightarrow> x = y"
 
 axiomatization pair where
-  pair_apply0: \<open>
-    (\<And>f g. (x \<cdot> f) o\<^sub>m (y \<cdot> g) = (y \<cdot> g) o\<^sub>m (x \<cdot> f)) \<Longrightarrow>
-    pair x y \<cdot> (f \<otimes> g) = (x \<cdot> f) o\<^sub>m (y \<cdot> g)\<close>
+  pair_apply: \<open>pair x y \<cdot> (f \<otimes> g) = (x \<cdot> f) o\<^sub>m (y \<cdot> g)\<close>
 
-(* TODO prove this *)
+axiomatization where
+  pair_is_lvalue0: 
+  \<open>\<lbrakk>is_lvalue x; is_lvalue y;
+    \<And>f g. (x \<cdot> f) o\<^sub>m (y \<cdot> g) = (y \<cdot> g) o\<^sub>m (x \<cdot> f)\<rbrakk>
+     \<Longrightarrow> is_lvalue (pair x y)\<close>
+
+(* (* TODO prove this *)
 axiomatization where compatible30:
 \<open>\<lbrakk>\<And>f g. (x \<cdot> f) o\<^sub>m (y \<cdot> g) = (y \<cdot> g) o\<^sub>m (x \<cdot> f);
   \<And>f h. (x \<cdot> f) o\<^sub>m (z \<cdot> h) = (z \<cdot> h) o\<^sub>m (x \<cdot> f);
   \<And>g h. (y \<cdot> g) o\<^sub>m (z \<cdot> h) = (z \<cdot> h) o\<^sub>m (y \<cdot> g)\<rbrakk>
-    \<Longrightarrow> (pair x y \<cdot> fg) o\<^sub>m (z \<cdot> h) = (z \<cdot> h) o\<^sub>m (pair x y \<cdot> fg)\<close>
+    \<Longrightarrow> (pair x y \<cdot> fg) o\<^sub>m (z \<cdot> h) = (z \<cdot> h) o\<^sub>m (pair x y \<cdot> fg)\<close> *)
 
 axiomatization left_tensor where
-  left_tensor_apply[simp]: "left_tensor f \<cdot> g = f \<otimes> g"
+  left_tensor_apply: "left_tensor f \<cdot> g = f \<otimes> g"
 
 axiomatization swap where
-  swap_apply[simp]: "swap \<cdot> (f \<otimes> g) = g \<otimes> f"
+  swap_apply: "swap \<cdot> (f \<otimes> g) = g \<otimes> f"
 
 axiomatization assoc where
-  assoc_apply[simp]: "assoc \<cdot> (f \<otimes> (g \<otimes> h)) = (f \<otimes> g) \<otimes> h"
+  assoc_apply: "assoc \<cdot> (f \<otimes> (g \<otimes> h)) = (f \<otimes> g) \<otimes> h"
 
 axiomatization assoc' where
-  assoc'_apply[simp]: "assoc' \<cdot> (f \<otimes> g) \<otimes> h = (f \<otimes> (g \<otimes> h))"
+  assoc'_apply: "assoc' \<cdot> (f \<otimes> g) \<otimes> h = (f \<otimes> (g \<otimes> h))"
 
 end
